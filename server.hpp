3af14294aaf0f65543c74b4140df13151b7057bd
vivek <vivekroy2584@gmail.com>
2025-05-24 05:19:32 +0000

    http server setup

---
+++ server.hpp
@@ -0,0 +1,15 @@
+#ifndef SERVER_HPP
+#define SERVER_HPP
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <cstring>
+#include <array>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+
+#endif
